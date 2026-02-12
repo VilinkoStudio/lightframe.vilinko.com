3460d12e53eda1f51d4767cfab711a7f0da0a5a7
xihale <xihale.top@qq.com>
2026-02-13 00:26:28 +0800

    fix(ui): refine interactive elements and accessibility
    - Fix visibility of acknowledgement icons on hover in dark mode
    - Remove hover transformation from contributor groups for a cleaner feel
    - Clean up unused navigation underline styles in header
    - Implement dynamic mouse-following spotlight effect in Intro cards

--- src/components/lightframe/intro/intro.tsx
+++ src/components/lightframe/intro/intro.tsx
@@ -1,4 +1,4 @@
-import { component$, useStylesScoped$ } from "@qwik.dev/core";
+import { component$, useStylesScoped$, $ } from "@qwik.dev/core";
 import { introConfig } from "~/config";
 import { Icon } from "~/components/common/Icon";
 import type { IntroView } from "~/types";
@@ -7,23 +7,37 @@ import styles from "./intro.css?inline";
 export default component$(() => {
   useStylesScoped$(styles);
 
+  const handleMouseMove = $((e: MouseEvent, currentTarget: HTMLElement) => {
+    const { left, top } = currentTarget.getBoundingClientRect();
+    const x = e.clientX - left;
+    const y = e.clientY - top;
+    currentTarget.style.setProperty("--mouse-x", `${x}px`);
+    currentTarget.style.setProperty("--mouse-y", `${y}px`);
+  });
+
   return (
     <div class="intro">
       <div class="intro-grid">
         {introConfig.views.map((item) => (
-          <IntroItem key={item.word} {...item} />
+          <IntroItem key={item.word} {...item} onMouseMove$={handleMouseMove} />
         ))}
       </div>
     </div>
   );
 });
 
-const IntroItem = component$<IntroView>(({ icon, word, intro }) => (
-  <div class="intro-item">
-    <div class="intro-icon">
-      <Icon paths={icon} width={28} height={28} />
+interface IntroItemProps extends IntroView {
+  onMouseMove$?: any;
+}
+
+const IntroItem = component$<IntroItemProps>(
+  ({ icon, word, intro, onMouseMove$ }) => (
+    <div class="intro-item" onMouseMove$={onMouseMove$}>
+      <div class="intro-icon">
+        <Icon paths={icon} width={24} height={24} />
+      </div>
+      <h3>{word}</h3>
+      <p>{intro}</p>
     </div>
-    <h3>{word}</h3>
-    <p>{intro}</p>
-  </div>
-));
+  ),
+);
